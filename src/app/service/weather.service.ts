845b0a8e3da92f1c4f79d9c6f1a6b6a0531963dc
Uros Novakovic <100865250+urosnovak@users.noreply.github.com>
2023-02-04 09:02:51 +0100

    Add files via upload

---
+++ src/app/service/weather.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { WeatherData } from 'src/models/weather.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>(`https://api.openweathermap.org/data/2.5/weather?zip=${cityName},us&APPID=22fba64f391acde0cf3dbe0487296500&units=metric` );
+  }
+
+
+  // getWeatherData(cityName:string):Observable<WeatherData>{
+  //   return this.http.get<WeatherData>(environment.weatherApiBaseUrl+'/city/'+cityName, {
+  //     headers: new HttpHeaders()
+  //     .set(environment.XRapidAPIHeaderName, environment.XRapidAPIHeaderValue)
+  //     .set(environment.XRapidAPIKeyHeaderName,environment.XRapidAPIKeyHeaderValue),
+  //     params: new HttpParams()
+  //     .set('q', cityName)
+  //     .set('units', 'metric')
+  //     .set('mode', 'json')
+  //   });
+  // }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://rapidapi.com/worldapi/api/open-weather13/',
+    XRapidAPIHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue:'ea7b0ae66dmsh515ccb36b68bf3fp16196bjsnc2d9b0d98b2a',
+
+
+};
